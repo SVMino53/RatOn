ca445e7caa769736382205de600a12045da772eb
Isak Forsberg <isak-forsberg@hotmail.se>
2020-04-08 03:09:05 +0200

    Controlling Player

---
+++ Assets/ControllerInput.cs
@@ -0,0 +1,190 @@
+// GENERATED AUTOMATICALLY FROM 'Assets/ControllerInput.inputactions'
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
+
+public class @ControllerInput : IInputActionCollection, IDisposable
+{
+    public InputActionAsset asset { get; }
+    public @ControllerInput()
+    {
+        asset = InputActionAsset.FromJson(@"{
+    ""name"": ""ControllerInput"",
+    ""maps"": [
+        {
+            ""name"": ""Gameplay"",
+            ""id"": ""19a6c5f3-0c0c-40a0-bcfe-72e97f9b9550"",
+            ""actions"": [
+                {
+                    ""name"": ""Move"",
+                    ""type"": ""Button"",
+                    ""id"": ""dee2d609-2e0d-4f3e-980a-8aa2167f1d08"",
+                    ""expectedControlType"": """",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""Run"",
+                    ""type"": ""Button"",
+                    ""id"": ""43192300-4473-4ea2-838e-9b3020ccb732"",
+                    ""expectedControlType"": """",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""StandStill"",
+                    ""type"": ""Button"",
+                    ""id"": ""97064bd1-03d9-4ada-af26-43b6585ddc27"",
+                    ""expectedControlType"": """",
+                    ""processors"": """",
+                    ""interactions"": """"
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": """",
+                    ""id"": ""4d2eb76c-2d2a-4012-9e8a-1b0215b8c09b"",
+                    ""path"": ""<Gamepad>/leftStick"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""126b0608-8013-42fd-be74-fffca0ed0c09"",
+                    ""path"": ""<Gamepad>/buttonWest"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Run"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""170cf205-ca1d-469a-9a1f-1c17067811a3"",
+                    ""path"": ""<Gamepad>/buttonSouth"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""StandStill"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                }
+            ]
+        }
+    ],
+    ""controlSchemes"": []
+}");
+        // Gameplay
+        m_Gameplay = asset.FindActionMap("Gameplay", throwIfNotFound: true);
+        m_Gameplay_Move = m_Gameplay.FindAction("Move", throwIfNotFound: true);
+        m_Gameplay_Run = m_Gameplay.FindAction("Run", throwIfNotFound: true);
+        m_Gameplay_StandStill = m_Gameplay.FindAction("StandStill", throwIfNotFound: true);
+    }
+
+    public void Dispose()
+    {
+        UnityEngine.Object.Destroy(asset);
+    }
+
+    public InputBinding? bindingMask
+    {
+        get => asset.bindingMask;
+        set => asset.bindingMask = value;
+    }
+
+    public ReadOnlyArray<InputDevice>? devices
+    {
+        get => asset.devices;
+        set => asset.devices = value;
+    }
+
+    public ReadOnlyArray<InputControlScheme> controlSchemes => asset.controlSchemes;
+
+    public bool Contains(InputAction action)
+    {
+        return asset.Contains(action);
+    }
+
+    public IEnumerator<InputAction> GetEnumerator()
+    {
+        return asset.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public void Enable()
+    {
+        asset.Enable();
+    }
+
+    public void Disable()
+    {
+        asset.Disable();
+    }
+
+    // Gameplay
+    private readonly InputActionMap m_Gameplay;
+    private IGameplayActions m_GameplayActionsCallbackInterface;
+    private readonly InputAction m_Gameplay_Move;
+    private readonly InputAction m_Gameplay_Run;
+    private readonly InputAction m_Gameplay_StandStill;
+    public struct GameplayActions
+    {
+        private @ControllerInput m_Wrapper;
+        public GameplayActions(@ControllerInput wrapper) { m_Wrapper = wrapper; }
+        public InputAction @Move => m_Wrapper.m_Gameplay_Move;
+        public InputAction @Run => m_Wrapper.m_Gameplay_Run;
+        public InputAction @StandStill => m_Wrapper.m_Gameplay_StandStill;
+        public InputActionMap Get() { return m_Wrapper.m_Gameplay; }
+        public void Enable() { Get().Enable(); }
+        public void Disable() { Get().Disable(); }
+        public bool enabled => Get().enabled;
+        public static implicit operator InputActionMap(GameplayActions set) { return set.Get(); }
+        public void SetCallbacks(IGameplayActions instance)
+        {
+            if (m_Wrapper.m_GameplayActionsCallbackInterface != null)
+            {
+                @Move.started -= m_Wrapper.m_GameplayActionsCallbackInterface.OnMove;
+                @Move.performed -= m_Wrapper.m_GameplayActionsCallbackInterface.OnMove;
+                @Move.canceled -= m_Wrapper.m_GameplayActionsCallbackInterface.OnMove;
+                @Run.started -= m_Wrapper.m_GameplayActionsCallbackInterface.OnRun;
+                @Run.performed -= m_Wrapper.m_GameplayActionsCallbackInterface.OnRun;
+                @Run.canceled -= m_Wrapper.m_GameplayActionsCallbackInterface.OnRun;
+                @StandStill.started -= m_Wrapper.m_GameplayActionsCallbackInterface.OnStandStill;
+                @StandStill.performed -= m_Wrapper.m_GameplayActionsCallbackInterface.OnStandStill;
+                @StandStill.canceled -= m_Wrapper.m_GameplayActionsCallbackInterface.OnStandStill;
+            }
+            m_Wrapper.m_GameplayActionsCallbackInterface = instance;
+            if (instance != null)
+            {
+                @Move.started += instance.OnMove;
+                @Move.performed += instance.OnMove;
+                @Move.canceled += instance.OnMove;
+                @Run.started += instance.OnRun;
+                @Run.performed += instance.OnRun;
+                @Run.canceled += instance.OnRun;
+                @StandStill.started += instance.OnStandStill;
+                @StandStill.performed += instance.OnStandStill;
+                @StandStill.canceled += instance.OnStandStill;
+            }
+        }
+    }
+    public GameplayActions @Gameplay => new GameplayActions(this);
+    public interface IGameplayActions
+    {
+        void OnMove(InputAction.CallbackContext context);
+        void OnRun(InputAction.CallbackContext context);
+        void OnStandStill(InputAction.CallbackContext context);
+    }
+}
--- Assets/Scripts/Player_scr.cs
+++ Assets/Scripts/Player_scr.cs
@@ -1,15 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player_scr : MonoBehaviour
 {
+    ControllerInput controls;
+
+    Vector2 faceDirection;
+    bool run;
+    bool standStill;
+
+
     public float walkSpeed = 1.0f;
     public float runSpeed = 1.5f;
 
     public float inputSensitivityOffset = 0.001f;
 
     Rigidbody2D rb;
+
+
+    private void Awake()
+    {
+        controls = new ControllerInput();
+
+        controls.Gameplay.Move.performed += ctx => faceDirection = ctx.ReadValue<Vector2>();
+        controls.Gameplay.Move.canceled += ctx => faceDirection = Vector2.zero;
+
+        controls.Gameplay.Run.performed += ctx => run = true;
+        controls.Gameplay.Run.canceled += ctx => run = false;
+
+        controls.Gameplay.StandStill.performed += ctx => standStill = true;
+        controls.Gameplay.StandStill.canceled += ctx => standStill = false;
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -20,17 +43,14 @@ public class Player_scr : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 direction = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0.0f);
-        direction.Normalize();
-
-        if (direction.x < -inputSensitivityOffset ||
-            direction.x > inputSensitivityOffset ||
-            direction.y < -inputSensitivityOffset ||
-            direction.y > inputSensitivityOffset)
+        if (faceDirection.x < -inputSensitivityOffset ||
+            faceDirection.x > inputSensitivityOffset ||
+            faceDirection.y < -inputSensitivityOffset ||
+            faceDirection.y > inputSensitivityOffset)
         {
-            float angle = Mathf.Asin(direction.x);
+            float angle = Mathf.Asin(faceDirection.normalized.x);
             angle *= -Mathf.Rad2Deg;
-            if (Input.GetAxis("Vertical") < 0.0f)
+            if (faceDirection.y < 0.0f)
             {
                 angle += 180.0f;
                 angle *= -1.0f;
@@ -39,17 +59,59 @@ public class Player_scr : MonoBehaviour
 
             transform.rotation = newRotation;
 
-            if (Input.GetAxis("Stand Still") <= 0.0f)
+            if (!standStill)
             {
-                if (Input.GetAxis("Run") > 0.0f)
+                if (run)
                 {
-                    transform.Translate(direction * runSpeed * Time.deltaTime, Space.World);
+                    transform.Translate(faceDirection * runSpeed * Time.deltaTime, Space.World);
                 }
                 else
                 {
-                    transform.Translate(direction * walkSpeed * Time.deltaTime, Space.World);
+                    transform.Translate(faceDirection * walkSpeed * Time.deltaTime, Space.World);
                 }
             }
         }
+        
+        //Vector3 direction = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0.0f);
+        //direction.Normalize();
+
+        //if (direction.x < -inputSensitivityOffset ||
+        //    direction.x > inputSensitivityOffset ||
+        //    direction.y < -inputSensitivityOffset ||
+        //    direction.y > inputSensitivityOffset)
+        //{
+        //    float angle = Mathf.Asin(direction.x);
+        //    angle *= -Mathf.Rad2Deg;
+        //    if (Input.GetAxis("Vertical") < 0.0f)
+        //    {
+        //        angle += 180.0f;
+        //        angle *= -1.0f;
+        //    }
+        //    Quaternion newRotation = Quaternion.Euler(0.0f, 0.0f, angle);
+
+        //    transform.rotation = newRotation;
+
+        //    if (Input.GetAxis("Stand Still") <= 0.0f)
+        //    {
+        //        if (Input.GetAxis("Run") > 0.0f)
+        //        {
+        //            transform.Translate(direction * runSpeed * Time.deltaTime, Space.World);
+        //        }
+        //        else
+        //        {
+        //            transform.Translate(direction * walkSpeed * Time.deltaTime, Space.World);
+        //        }
+        //    }
+        //}
+    }
+
+    private void OnEnable()
+    {
+        controls.Gameplay.Enable();
+    }
+
+    private void OnDisable()
+    {
+        controls.Gameplay.Disable();
     }
 }
