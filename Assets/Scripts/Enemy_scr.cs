66d71f22d540ca5425d238f5f49a9876d83f6e8d
Isak Forsberg <isak-forsberg@hotmail.se>
2020-05-04 13:33:04 +0200

    Path Finding Basically Working

--- Assets/Scripts/Enemy_scr.cs
+++ Assets/Scripts/Enemy_scr.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Enemy_scr : MonoBehaviour
 {
+    public bool testing = false;
+
     public enum State
     {
         IDLE,
@@ -32,12 +34,16 @@ public class Enemy_scr : MonoBehaviour
     public GameObject lineSightObj;
 
     public float getPathDelay = 0.5f;
-    float curGetPathDelay = 0.0f;
+    //float curGetPathDelay = 0.0f;
 
     public float nextPointDistence = 0.2f;
     int pointIndex = 0;
 
+    public Collider2D obstacleMapCollider;
+    public Collider2D windowMapCollider;
+
     public EdgeCollider2D generalLineCollider;
+    public CircleCollider2D generalPointCollider;
 
     EdgeCollider2D lineCollider;
 
@@ -48,28 +54,29 @@ public class Enemy_scr : MonoBehaviour
     // For testing
     public LineRenderer pathLine;
     LineRenderer prevPathLine = null;
+    public LineRenderer longPathLine;
+    LineRenderer prevLongPathLine = null;
 
-    bool GetLineIsColliding(Vector2 start, Vector2 end, string collidingTag)
+    bool GetLineIsCollidingWith(Vector2 start, Vector2 end, Collider2D collider, float pointDistance)
     {
-        generalLineCollider.transform.position = start;
-
         Vector2 direction = (end - start).normalized;
-        float angle = Vector2.SignedAngle(Vector2.up, direction);
+        Vector2 curPoint = start;
 
-        generalLineCollider.transform.rotation = Quaternion.Euler(0.0f, 0.0f, angle);
-
-        float distance = Vector2.Distance(start, end);
-        generalLineCollider.transform.localScale = new Vector3(1.0f, distance, 1.0f);
-
-        List<Collider2D> colliders = new List<Collider2D>();
-        int n = generalLineCollider.OverlapCollider(cf.NoFilter(), colliders);
-
-        for (int i = 0; i < n; i++)
+        int limit = 0;
+        while ((end - curPoint).magnitude > pointDistance)
         {
-            if (colliders[i].CompareTag(collidingTag))
+            if (limit == 1000)
+            {
+                break;
+            }
+            limit++;
+
+            if (collider.OverlapPoint(curPoint))
             {
                 return true;
             }
+
+            curPoint += direction * pointDistance;
         }
 
         return false;
@@ -77,56 +84,153 @@ public class Enemy_scr : MonoBehaviour
 
     List<Vector2> GetPath(Vector2 target)
     {
+        Vector2 curTarget = target;
+
+        List<Vector2> branchPoints = new List<Vector2>();
+
         if (prevPathLine != null)
         {
             Destroy(prevPathLine.gameObject);
         }
-        Vector2 curPoint = new Vector2(Mathf.Round(transform.position.x), Mathf.Round(transform.position.y));
 
-        List<Vector2> path = new List<Vector2>();
-        Vector2 newPoint;
+        if (prevLongPathLine != null)
+        {
+            Destroy(prevLongPathLine.gameObject);
+        }
+
+        Vector2 startPoint = new Vector2(Mathf.Round(transform.position.x), Mathf.Round(transform.position.y));
+        Vector2 curPoint = startPoint;
+
+        List <Vector2> path = new List<Vector2>();
 
         // For testing ->
         List<Vector3> pointsList = new List<Vector3>
         {
             curPoint
         };
+
+        List<Vector3> longPointsList = new List<Vector3>
+        {
+            curPoint
+        };
         // <-
 
-        while (Vector2.Distance(curPoint, target) > Mathf.Sqrt(2.0f) && Time.deltaTime < 10.0f)
+        List<Vector2> reachedPoints = new List<Vector2>();
+        List<Vector2> allPathPoints = new List<Vector2>();
+        int limit0 = 0;
+        while (Vector2.Distance(curPoint, target) > Mathf.Sqrt(2.0f))
         {
-
-            Vector2 pointToPlayer = playerObj.transform.position;
-            pointToPlayer -= curPoint;
-
-            if (pointToPlayer.x <= -1.0f)
+            if (limit0 >= 1000)
             {
-                curPoint.x -= 1.0f;
-            }
-            else if (pointToPlayer.x >= 1.0f)
-            {
-                curPoint.x += 1.0f;
-            }
-            if (pointToPlayer.y <= -1.0f)
-            {
-                curPoint.y -= 1.0f;
-            }
-            else if (pointToPlayer.y >= 1.0f)
-            {
-                curPoint.y += 1.0f;
+                break;
             }
 
-            path.Add(curPoint);
-            pointsList.Add(curPoint);
+            limit0++;
+
+            Vector2 nextPoint = curPoint;
+            List<Vector2> potentialPoints = new List<Vector2>();
+
+            reachedPoints.Add(curPoint);
+
+            nextPoint.y += 1.0f;
+            if (!obstacleMapCollider.OverlapPoint(nextPoint) && !reachedPoints.Contains(nextPoint))
+            {
+                potentialPoints.Add(nextPoint);
+            }
+            nextPoint.y -= 1.0f;
+            nextPoint.x += 1.0f;
+            if (!obstacleMapCollider.OverlapPoint(nextPoint) && !reachedPoints.Contains(nextPoint))
+            {
+                potentialPoints.Add(nextPoint);
+            }
+            nextPoint.y -= 1.0f;
+            nextPoint.x -= 1.0f;
+            if (!obstacleMapCollider.OverlapPoint(nextPoint) && !reachedPoints.Contains(nextPoint))
+            {
+                potentialPoints.Add(nextPoint);
+            }
+            nextPoint.y += 1.0f;
+            nextPoint.x -= 1.0f;
+            if (!obstacleMapCollider.OverlapPoint(nextPoint) && !reachedPoints.Contains(nextPoint))
+            {
+                potentialPoints.Add(nextPoint);
+            }
+
+            if (potentialPoints.Count > 0)
+            {
+                curPoint = potentialPoints[0];
+            }
+            
+            for (int i = 1; i < potentialPoints.Count; i++)
+            {
+                if (Vector2.Distance(potentialPoints[i], target) < Vector2.Distance(curPoint, target))
+                {
+                    curPoint = potentialPoints[i];
+                }
+            }
+
+            if (reachedPoints.Contains(curPoint))
+            {
+                if (allPathPoints.Count > 0)
+                {
+                    curPoint = allPathPoints[allPathPoints.Count - 1];
+                    allPathPoints.RemoveAt(allPathPoints.Count - 1);
+                }
+            }
+            else
+            {
+                allPathPoints.Add(curPoint);
+            }
         }
 
-        path.Add(playerObj.transform.position);
-        pointsList.Add(playerObj.transform.position);
+        Vector2 curReachablePoint = transform.position;
 
-        pathLine.positionCount = pointsList.Count;
-        pathLine.SetPositions(pointsList.ToArray());
+        if (allPathPoints.Count > 0)
+        {
+            int limit1 = 0;
+            while (curReachablePoint != allPathPoints[allPathPoints.Count - 1])
+            {
+                if (limit1 >= 1000)
+                {
+                    break;
+                }
+                limit1++;
 
-        prevPathLine = Instantiate(pathLine);
+                for (int i = allPathPoints.Count - 1; i >= 0; i--)
+                {
+                    if (!GetLineIsCollidingWith(curReachablePoint, allPathPoints[i], obstacleMapCollider, 0.1f))
+                    {
+                        curReachablePoint = allPathPoints[i];
+                        path.Add(curReachablePoint);
+                        break;
+                    }
+                }
+            }
+        }
+        path.Add(target);
+
+        if (testing)
+        {
+            for (int i = 0; i < path.Count; i++)
+            {
+                pointsList.Add(path[i]);
+            }
+
+            for (int i = 0; i < allPathPoints.Count; i++)
+            {
+                longPointsList.Add(allPathPoints[i]);
+            }
+
+            pathLine.positionCount = pointsList.Count;
+            pathLine.SetPositions(pointsList.ToArray());
+
+            longPathLine.positionCount = longPointsList.Count;
+            longPathLine.SetPositions(longPointsList.ToArray());
+
+            prevPathLine = Instantiate(pathLine);
+
+            prevLongPathLine = Instantiate(longPathLine);
+        }
 
         return path;
     }
@@ -140,6 +244,8 @@ public class Enemy_scr : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Vector2 position2d = new Vector2(transform.position.x, transform.position.y);
+
         if (curState == State.IDLE)
         {
 
@@ -150,34 +256,37 @@ public class Enemy_scr : MonoBehaviour
         }
         else if (curState == State.CHASING)
         {
-            if (curGetPathDelay >= getPathDelay)
+            if (path.Count > 0)
             {
-                path = GetPath(playerObj.transform.position);
+                float enemyToPathPointDistance = (path[pointIndex] - position2d).magnitude;
 
-                pointIndex = 0;
+                if (enemyToPathPointDistance <= nextPointDistence)
+                {
+                    if (pointIndex < path.Count - 1)
+                    {
+                        pointIndex++;
+                    }
+                    else
+                    {
+                        path = GetPath(playerObj.transform.position);
 
-                curGetPathDelay = 0.0f;
+                        pointIndex = 0;
+
+                        //curGetPathDelay = 0.0f;
+                    }
+                }
+
+                Vector2 goToPoint = path[pointIndex];
+
+                Vector2 direction = -transform.position;
+                direction += goToPoint;
+
+                transform.Translate(direction.normalized * runSpeed * Time.deltaTime, Space.World);
+
+                float angle = Vector2.SignedAngle(Vector2.up, direction.normalized);
+
+                transform.rotation = Quaternion.Euler(0.0f, 0.0f, angle);
             }
-            else
-            {
-                curGetPathDelay += Time.deltaTime;
-            }
-
-            if (Vector2.Distance(transform.position, path[pointIndex]) <= nextPointDistence && pointIndex < path.Count - 1)
-            {
-                pointIndex++;
-            }
-
-            Vector2 goToPoint = path[pointIndex];
-
-            Vector2 direction = -transform.position;
-            direction += goToPoint;
-
-            transform.Translate(direction.normalized * runSpeed * Time.deltaTime, Space.World);
-
-            float angle = Vector2.SignedAngle(Vector2.up, direction.normalized);
-
-            transform.rotation = Quaternion.Euler(0.0f, 0.0f, angle);
         }
         else if (curState == State.LOSING)
         {
@@ -220,27 +329,43 @@ public class Enemy_scr : MonoBehaviour
                 {
                     curState = State.CHASING;
 
-                    curGetPathDelay = getPathDelay;
+                    path = GetPath(playerObj.transform.position);
+
+                    pointIndex = 0;
+
+                    //curGetPathDelay = getPathDelay;
 
                     // FOR TESTING!!!
-                    GetComponent<SpriteRenderer>().color = new Color(1.0f, 0.0f, 0.0f);
+                    if (testing)
+                    {
+                        GetComponent<SpriteRenderer>().color = new Color(1.0f, 0.0f, 0.0f);
+                    }
                 }
                 else
                 {
                     // FOR TESTING!!!
-                    GetComponent<SpriteRenderer>().color = new Color(1.0f, 1.0f, 1.0f);
+                    if (testing)
+                    {
+                        GetComponent<SpriteRenderer>().color = new Color(1.0f, 1.0f, 1.0f);
+                    }
                 }
             }
             else
             {
                 // FOR TESTING!!!
-                GetComponent<SpriteRenderer>().color = new Color(1.0f, 1.0f, 1.0f);
+                if (testing)
+                {
+                    GetComponent<SpriteRenderer>().color = new Color(1.0f, 1.0f, 1.0f);
+                }
             }
         }
         else
         {
             // FOR TESTING!!!
-            GetComponent<SpriteRenderer>().color = new Color(1.0f, 1.0f, 1.0f);
+            if (testing)
+            {
+                GetComponent<SpriteRenderer>().color = new Color(1.0f, 1.0f, 1.0f);
+            }
         }
     }
 }
